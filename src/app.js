490e7149b7b19bb2d2b8a3509d4e1509739aecf2
Brandon <brandoncyu@gmail.com>
2019-07-10 13:15:28 -0700

    added read me

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('./db/mongoose');
+// require('./db/mongoose');
 const userRouter = require('./routes/users');
 const taskRouter = require('./routes/tasks');
 
